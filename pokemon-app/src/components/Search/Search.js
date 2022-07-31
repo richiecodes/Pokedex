c020090cdc27fa51aa0adf916c45c21cfb00aae0
Richard DeLuca <richard.deluca@aol.com>
2022-07-31 16:54:49 -0400

    working console log for GET method

--- pokemon-app/src/components/Search/Search.js
+++ pokemon-app/src/components/Search/Search.js
@@ -1,11 +1,28 @@
 import {useState} from 'react'
 
+const API_URL = 'http://localhost:8080/pokemon/search/name/'
 
 const Search = () => {
+  const [search, setSearch] = useState("");
+  const [pokemon, setPokemon] = useState({});
+
+  const getSearch = async (e) => {
+    e.preventDefault();
+    const response = await fetch(API_URL + search);
+    const data = await response.json();
+
+    setPokemon(data);
+
+    console.log(pokemon);
+
+  }
 
   return (
     <div>
-      <h1>Search</h1>
+      <form onSubmit={getSearch}>
+        <label>By Name: &nbsp;</label>
+        <input type='text' placeholder='Search Pokemon' value={search} onChange={(e) => setSearch(e.target.value)} />
+      </form>
     </div>
   )
 }
